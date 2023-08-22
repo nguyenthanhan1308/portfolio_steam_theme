a58683a338da19dc866ac6ed8441f9a783465f12
THANHAN <sunthanhan1308@gmail.com>
2023-08-22 10:19:22 +0700

    set up header footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function FooterSection() {
+  return (
+    <div className="absolute bottom-0 flex justify-center" style={{width: "-webkit-fill-available"}}>footer</div>
+  )
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export function HeaderSection() {
+	return (
+		<div className="flex justify-between">
+			<div className="flex gap-x-3">
+				<Link href="">Store</Link>
+				<Link href="">Library</Link>
+				<Link href="">Community</Link>
+			</div>
+			<div className="flex gap-x-3">
+				<div className="flex bg-lime-700">
+					<Image
+						src=""
+						alt="profile avatar"
+						width={32}
+						height={32}
+					/>
+					<p className="text-lime-500">
+						Nguyen Thanh An
+					</p>
+				</div>
+				<div className="flex gap-x-3">
+					<button>_</button>
+					<button>o</button>
+					<button>x</button>
+				</div>
+			</div>
+		</div>
+	);
+}
