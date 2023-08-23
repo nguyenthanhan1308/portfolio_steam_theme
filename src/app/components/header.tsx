c5209d9d93720159c2deec75def75e1317d95c30
THANHAN <sunthanhan1308@gmail.com>
2023-08-23 16:14:32 +0700

    [23/8] - Theme provider added

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,15 +1,18 @@
+'use client'
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-
+import { ArrowDropDown, ArrowBack, ArrowForward, Close, Minimize } from "@mui/icons-material";
 export function HeaderSection() {
 	return (
 		<div className="flex justify-between">
-			<div className="flex gap-x-3">
+			<nav className="flex gap-x-3">
+				<ArrowBack/>
+				<ArrowForward/>
 				<Link href="/store">Store</Link>
 				<Link href="/library">Library</Link>
 				<Link href="/community">Community</Link>
-			</div>
+			</nav>
 			<div className="flex gap-x-3">
 				<div className="flex bg-lime-700">
 					<Image
@@ -21,11 +24,14 @@ export function HeaderSection() {
 					<p className="text-lime-500">
 						Nguyen Thanh An
 					</p>
+					<ArrowDropDown
+
+					/>
 				</div>
 				<div className="flex gap-x-3">
-					<button>_</button>
+					<Minimize/>
 					<button>o</button>
-					<button>x</button>
+					<Close/>
 				</div>
 			</div>
 		</div>
